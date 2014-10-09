954ac827abd7614ee3fba2cd23251835eb54f4a0
Kheang Lim <kheang@gmail.com>
2014-10-09 10:28:44 -0400

    Removed unnecessary def of time_ago_in_words helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-  def time_ago_in_words(from_time, include_seconds_or_options = {})
-    distance_of_time_in_words(from_time, Time.now, include_seconds_or_options)
-  end
+  # def time_ago_in_words(from_time, include_seconds_or_options = {})
+  #   distance_of_time_in_words(from_time, Time.now, include_seconds_or_options)
+  # end
 end
