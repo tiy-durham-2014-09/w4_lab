d7e2a221a6d17b9d64cbeaf2167ca9dbb1ea149f
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-10-09 15:42:26 -0400

    Added to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-    def current_user
-        @current_user ||= User.find_by(id: session[current_user_id])
-    end
+  helper_method :current_user, :logged_in?
 
-    def logged_in?
-      current_user
-    end
+  private
 
-    def authenticate
-	    current_user_id = session[:current_user_id]
-	    unless current_user_id
-        redirect_to new_login_path, notice: "You must be logged in to see that."
-      end
+  def current_user
+      @current_user ||= User.find_by(id: session[:current_user_id])
+  end
+
+  def logged_in?
+    current_user
+  end
+
+  def authenticate
+    current_user_id = session[:current_user_id]
+    unless current_user_id
+      redirect_to new_login_path, notice: "You must be logged in to see that."
     end
+  end
 
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :authenticate
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
   # GET /todos
