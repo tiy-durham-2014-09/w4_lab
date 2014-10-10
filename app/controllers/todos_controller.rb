92e562cb4984613269d50906e06eb841e487bc26
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-10-10 16:45:32 -0400

    Changed checkbox

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,7 +31,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Todo was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
