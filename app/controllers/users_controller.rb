fe95bd3e2f8a6581d47ac6c78c512110476cd949
Kheang Lim <kheang@gmail.com>
2014-10-09 23:21:58 -0400

    Linked user and profile creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        session[:current_user_id] = @user.id
+        format.html { redirect_to new_profile_path notice: 'User was successfully created. Fill in profile details to complete registration.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
