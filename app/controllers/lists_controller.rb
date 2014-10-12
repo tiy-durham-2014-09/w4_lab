ee633bcbcbb96a43e9030d9934432361bed11d13
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-10-12 15:44:45 -0400

    Fixed todo edit bug

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,7 +13,7 @@ class ListsController < ApplicationController
   # GET /lists/1.json
   def show
 
-    @list = List.find(params[:list_id])
+    @list = List.find(params[:id])
   end
 
   # GET /lists/new
@@ -32,7 +32,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.html { redirect_to root_path, notice: 'List was successfully created.' }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
