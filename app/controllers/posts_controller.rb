38a911ca2881eb6671640f223f351a6542e3f915
Danielle <danielleheckman5@gmail.com>
2014-10-11 20:20:23 -0400

    nearly finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,42 +2,18 @@ class PostsController < ApplicationController
   before_action :authenticate
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :ensure_logged_in, only: [:new]
-  # after_action :ensure_user_owns_post, only: [:new]
-  # GET /posts
-  # GET /posts.json
-  # def index
-  #   @posts = current_user.posts
-  # end
+
   def index
     @posts = Post.order("published_date DESC")
   end
-
   def show
     @post = Post.find(params[:id])
   end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  # def show
-  # end
-
-  # GET /posts/new
   def new
     @post = Post.new
   end
-
-  # GET /posts/1/edit
   def edit
-      # @post = current_user.posts.find{|p| p.id == params[:id] }
   end
-    # if current_user
-    #   @post.update(post_params)
-    # else
-    #   redirect_to post_path, flash: {alert: "Sorry, you can't edit other users' posts."}
-    # end
-
-  # POST /posts
-  # POST /posts.json
   def create
     if current_user
       @post = current_user.posts.build(post_params)
@@ -50,9 +26,6 @@ class PostsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -64,9 +37,6 @@ class PostsController < ApplicationController
       end
     end
   end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -80,20 +50,17 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
-
   def ensure_user_owns_post
     if @post.user != current_user
       render nothing: true, status: :not_found
       redirect_to root_path, flash: {alert: "You tried to access an entry that doesn't belong to you."}
     end
   end
-
   def ensure_logged_in
     if current_user == nil
       redirect_to root_path, flash: {alert: "You must log in to make a post."}
     end
   end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:post).permit(:title, :body,:published_date)
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Post.delete_all
 
-Post.create(
-  title: "My Very First Post",
-  body:
-  %Q{### There Is Something You Should Know!
-
-  This is my very first post using markdown!
-
-  How do you like it?  I learned this from [RichOnRails.com](http://richonrails.com/articles/rendering-markdown-with-redcarpet)!},
-  published_date: Date.current - 1.day,
-
-)
-
-Post.create(
-  title: "My Second Post",
-  published_date: Date.current - 1.day,
-  body:
-  %Q{### My List of Things To Do!
-
-  Here is the list of things I wish to do!
-
-  * write more posts
-  * write even more posts
-  * write even more posts!}
-)
+# Post.create(
+#   title: "My Very First Post",
+#   body:
+#   %Q{### There Is Something You Should Know!
+#
+#   This is my very first post using markdown!
+#
+#   How do you like it?  I learned this from [RichOnRails.com](http://richonrails.com/articles/rendering-markdown-with-redcarpet)!},
+#   user: 'me',
+#   published_date: Date.current - 1.day,
+#
+# )
+#
+# Post.create(
+#   title: "My Second Post",
+#   body:
+#   %Q{### My List of Things To Do!
+#
+#   Here is the list of things I wish to do!
+#
+#   * write more posts
+#   * write even more posts
+#   * write even more posts!},
+#   published_date: Date.current - 1.day
+# )
