63635f41db798bb30779a8253c9f6bbbf6461ca2
Will <wrussell35@gmail.com>
2014-10-12 22:43:06 -0400

    shows incomplete vs. complete list items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,9 @@ class Item < ActiveRecord::Base
 
   validates :content, presence: true
 
+  scope :complete, -> {where("completed_by is not null")}
+  scope :incomplete, -> {where(completed_by: nil)}
+
   def completed?
     !completed_by.blank?
   end
--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,12 @@ class List < ActiveRecord::Base
   validates :title, presence: true
   validates :title, length: {minimum: 3}
   validates :description, presence: true
+
+  def has_completed_items?
+    items.complete.size > 0
+  end
+
+  def has_incomplete_items?
+    items.incomplete.size > 0
+  end
 end
