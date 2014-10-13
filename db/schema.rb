8182713b37566e167e93b523deec7bdd4e47d12d
Will <wrussell35@gmail.com>
2014-10-12 23:15:19 -0400

    user model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013012138) do
+ActiveRecord::Schema.define(version: 20141013025514) do
 
   create_table "items", force: true do |t|
     t.integer  "list_id"
@@ -29,5 +29,15 @@ ActiveRecord::Schema.define(version: 20141013012138) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
